ce6f670187d0171bc659078f2d62e7948cc6e8f8
YangXin <yangxinhust@hotmail.com>
2015-03-17 15:17:02 +0800

    google search

--- hello.py
+++ hello.py
@@ -8,15 +8,24 @@ def application(environ,start_response):
         url=str(environ["PATH_INFO"])+"?"+str(environ["QUERY_STRING"])
     except:
         return "Wrong"
-    google="https://translate.google.com"
+    google="https://www.google.com"
     res=urllib2.urlopen(google+url)
     html=res.read()
     status='200 OK'
-    response_headers = [('Content-Type', 'text/plain'),('Content-Length', str(len(html)))]
+    response_headers = [('Content-Type', 'text/html'),('Content-Length', str(len(html)))]
     start_response(status, response_headers)
 
     return html
 
+def app(environ,start_response):
+    res=urllib2.urlopen("http://www.baidu.com")
+    result=res.read()
+
+    status='200 OK'
+    response_headers = [('Content-Type', 'text/html'),('Content-Length', str(len(result)))]
+    start_response(status, response_headers)
+
+    return result
 port=int(os.environ.get("PORT",5000))
 httpd=make_server('0.0.0.0',port,application)
 httpd.serve_forever()
