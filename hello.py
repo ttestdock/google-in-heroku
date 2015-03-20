426b2779012ab9b90d787635249824bf23d77e22
YangXin <yangxinhust@hotmail.com>
2015-03-20 23:04:26 +0800

    google proxy

--- hello.py
+++ hello.py
@@ -2,25 +2,26 @@ import os
 from wsgiref.simple_server import make_server
 import urllib2
 def application(environ,start_response):
-    try:
-        requestUrl=str(environ["PATH_INFO"])+"?"+str(environ["QUERY_STRING"])
-    except:
-        return "Error!"
-    
-    try:
-        userAgent=str(environ["HTTP_USER_AGENT"])
-    except:
-        userAgent="Mozilla/5.0"
 
     googleUrl="https://www.google.com"
-    
+
+    try:
+        userAgent=str(environ["HTTP_USER_AGENT"])
+        requestUrl=str(environ["PATH_INFO"])+"?"+str(environ["QUERY_STRING"])
+        httpAccept=str(environ["HTTP_ACCEPT"]) 
+    except:
+        userAgent="Mozilla/5.0"
+        requestUrl=""
+
+
     try:
         opener=urllib2.build_opener()
         opener.addheaders=[('User-agent', userAgent)]
+        opener.addheaders=[('Accept',httpAccept)]
         html=opener.open(googleUrl+requestUrl).read()
     except:
-        html="something wrong"
-    
+        html="Can not open the page.Please retry in a few minutes"
+
     status='200 OK'
     response_headers = [('Content-Type', 'text/html;charset=utf-8'),('Content-Length', str(len(html)))]
     start_response(status, response_headers)
