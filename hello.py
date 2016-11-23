4d8d37f504a14a10a74eb1fa1beec362b56b8e60
ttestdock <2718648523@qq.com>
2016-11-23 13:03:34 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -19,17 +19,3 @@ def application(environ,start_response):
         opener.addheaders=[('User-agent', userAgent)]
         opener.addheaders=[('Accept',httpAccept)]
         html=opener.open(googleUrl+requestUrl).read()
-    except:
-        html="Can not open the page.Please retry in a few minutes"
-
-    status='200 OK'
-    response_headers = [('Content-Type', 'text/html;charset=utf-8'),('Content-Length', str(len(html)))]
-    start_response(status, response_headers)
-
-    return html
-
-port=int(os.environ.get("PORT",5000))
-httpd=make_server('0.0.0.0',port,application)
-httpd.serve_forever()
-
-
